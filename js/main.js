7596572702f890c6afe8a0e900da2f86074a9770
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-22 16:48:32 +0100

    Text and Title on Slide Done

--- js/main.js
+++ js/main.js
@@ -5,22 +5,24 @@ const container = document.querySelector(".item");
 const buttonUp = document.querySelector(".circle.up");
 const buttonDown = document.querySelector(".circle.down");
 const classImg = document.getElementsByClassName("img-carousel");
+const card = document.getElementById("card");
+const title = document.getElementById("title");
+const text = document.getElementById("text");
 
 // Array di oggetti 
 const images = [ { image: 'img/01.webp', title: 'Marvel\'s Spiderman Miles Morale', text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.', }, { image: 'img/02.webp', title: 'Ratchet & Clank: Rift Apart', text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.', }, { image: 'img/03.webp', title: 'Fortnite', text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.", }, { image: 'img/04.webp', title: 'Stray', text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city', }, { image: 'img/05.webp', title: "Marvel's Avengers", text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.', } ];
 
-let imgArray = [];
-images.forEach(element => {
-    imgArray.push(element.image)
+// Stampare immagini con ciclo for each
+images.forEach(element =>{
+    let newElement = document.createElement("img");
+    card.append(newElement);
+    newElement.src = element.image;
+    newElement.classList.add("img-carousel");
+    
 });
 
-// Stampare immagini con ciclo for each
-imgArray.forEach(element =>{
-    let newElement = document.createElement("img");
-    container.append(newElement);
-    newElement.src = element;
-    newElement.classList.add("img-carousel");
-});
+title.innerHTML = images[0].title;
+text.innerHTML = images[0].text;
 
 // Aggiunta classe per rendere visibile la prima slide
 classImg[0].classList.add("active")
@@ -34,7 +36,10 @@ buttonDown.addEventListener("click",
     function(){
         console.log("ciao");
         nSlide++;
-   
+
+        title.innerHTML = images[nSlide].title;
+        text.innerHTML = images[nSlide].text;
+        
         classImg[nSlide].classList.add("active")
         classImg[(nSlide - 1)].classList.remove("active")
 
@@ -58,8 +63,9 @@ buttonUp.addEventListener("click",
         
         if (nSlide === 0){
             buttonUp.classList.add("hidden")
+            buttonDown.classList.remove("hidden")
             
-        } else if (nSlide === 3){
+        } else if (nSlide === 4){
             buttonDown.classList.remove("hidden")
         }
     }
